1ae866e68a11b450308caf6d39c1800fa7c1b4d2
Artur Dias <arturdrm@hotmail.com>
2023-08-14 20:54:15 -0300

    ADDED: OpcNetApi

---
+++ OpcDaNetCore/Models/OpcDaServer.cs
@@ -0,0 +1,15 @@
+﻿namespace OpcDaNetCore.Models;
+
+public class OpcDaServer
+{
+    public OpcDaServer(string serverName, string host, string url)
+    {
+        ServerName = serverName;
+        Host = host;
+        Url = url;
+    }
+
+    public string ServerName { get; set; }
+    public string Host { get; set; }
+    public string Url { get; set; }
+}
---
+++ OpcDaNetCore/Utilities/BrowseOpcDaServers.cs
@@ -0,0 +1,17 @@
+﻿using Opc;
+using OpcDaNetCore.Models;
+
+namespace OpcDaNetCore.Utilities;
+
+public static class BrowseOpcDaServers
+{
+    public static IEnumerable<OpcDaServer> BrowseServers(string ip)
+    {
+        ArgumentNullException.ThrowIfNull(ip, "Invalid server ip");
+
+        using IDiscovery discovery = new OpcCom.ServerEnumerator();
+        var enumetare = discovery.GetAvailableServers(Specification.COM_DA_20, ip, null);
+
+        return enumetare.Select(i => new OpcDaServer(i.Name, i.Url.HostName, i.Url.ToString()));
+    }
+}
